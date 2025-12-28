53c51c1e83037421535a20679b243285f77a20c6
vivekcoder01 <vk612004@gmail.com>
2025-12-28 10:08:57 +0530

    commit update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log("🔥 Firebase config loaded");
-
 const firebaseConfig = {
-  apiKey: "PASTE_FROM_FIREBASE",
-  authDomain: "PASTE_FROM_FIREBASE",
-  projectId: "PASTE_FROM_FIREBASE",
-  storageBucket: "PASTE_FROM_FIREBASE",
-  messagingSenderId: "PASTE_FROM_FIREBASE",
-  appId: "PASTE_FROM_FIREBASE"
+  apiKey: "AIzaSyAU0P8b668j8QdlrwMtHuFSlB1nsDX77YI",
+  authDomain: "neocart-de5d8.firebaseapp.com",
+  projectId: "neocart-de5d8",
+  storageBucket: "neocart-de5d8.appspot.com",
+  messagingSenderId: "778513854488",
+  appId: "1:778513854488:web:0c572275696aab1543936d"
 };
 
+// 🔥 ONLY ONCE
 const app = initializeApp(firebaseConfig);
 
+// exports
 export const auth = getAuth(app);
 export const db = getFirestore(app);
